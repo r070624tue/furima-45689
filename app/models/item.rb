f01f936b616c68e2f4d85e4f564fb0fa727805d7
r070624tue <h290404tue@gmail.com>
2025-07-28 15:43:48 +0900

    モデルの作成と関連付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,28 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  has_one_attached :image
+  # has_one :order
+
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is invalid" }
+
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
+  end
 end
