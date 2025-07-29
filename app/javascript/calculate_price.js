8df81e350069c03753e97ce7050e6de108302114
r070624tue <h290404tue@gmail.com>
2025-07-29 15:48:04 +0900

    JavaScript修正その3

--- app/javascript/calculate_price.js
+++ app/javascript/calculate_price.js
@@ -1,14 +1,20 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('turbo:load', function(){
   const priceInput = document.getElementById('item-price');
 
   if (priceInput) {
     priceInput.addEventListener('input', () => {
       const price = parseInt(priceInput.value);
-      const commission = Math.floor(price * 0.1);
-      const profit = price - commission;
 
-      document.getElementById('add-tax-price').textContent = commission;
-      document.getElementById('profit').textContent = profit;
+      if (!isNaN(price) && price >= 300 && price <= 9999999) {
+        const commission = Math.floor(price * 0.1);
+        const profit = price - commission;
+
+        document.getElementById('add-tax-price').textContent = commission;
+        document.getElementById('profit').textContent = profit;
+      } else {
+        document.getElementById('add-tax-price').textContent = 0;
+        document.getElementById('profit').textContent = 0;
+      }
     });
   }
 });
