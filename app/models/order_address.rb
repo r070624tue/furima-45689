1a05c8da6dd1477b9c2d3bafbbb3eb0f12e4caa9
r070624tue <h290404tue@gmail.com>
2025-08-01 18:30:12 +0900

    Formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :token,
+                :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :postal_code,  format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    order = Order.create!(user_id: user_id, item_id: item_id)
+    Address.create!(
+      postal_code: postal_code, prefecture_id: prefecture_id, city: city,
+      addresses: addresses, building: building, phone_number: phone_number,
+      order_id: order.id
+    )
+  end
+end
