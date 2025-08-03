3709f00600b8e599eb2f68770d2861921d058259
r070624tue <h290404tue@gmail.com>
2025-08-03 20:36:58 +0900

    公開鍵の代入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   before_action :redirect_if_cannot_purchase
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
