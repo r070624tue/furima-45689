7043c95913ed48ffa5e2effaa0d0012425091338
r070624tue <h290404tue@gmail.com>
2025-08-03 20:29:51 +0900

    決済処理の記入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -31,6 +32,15 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address)
           .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
-          .merge(user_id: current_user.id, item_id: @item.id)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
